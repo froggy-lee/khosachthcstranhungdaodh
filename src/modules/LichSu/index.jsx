25b1c8f6a4f263bf8cce7e9c614f071da04d5a15
Le Hieu <firephoenix.shinji@gmail.com>
2023-11-24 03:19:26 +0700

    Lich su Viet Nam

--- src/modules/LichSu/index.jsx
+++ src/modules/LichSu/index.jsx
@@ -1,11 +1,59 @@
 import React from "react";
+import Link from "next/link";
 
 function LichSu() {
   return (
     <main className="container my-5">
       <h3>Lịch sử Việt Nam</h3>
+      {total.map((item) => {
+        return (
+          <div key={item.id}>
+            <Link
+              href={`/pdf-book/history/lich-su-viet-nam-bang-tranh-tap-${item.id}.pdf`}
+              target="_blank"
+            >
+              Lịch sử Việt Nam tập {item.id} - {item.title}
+            </Link>
+          </div>
+        );
+      })}
     </main>
   );
 }
 
 export default LichSu;
+
+const total = [
+  {
+    id: 1,
+    title: "Thời Hùng Vương",
+  },
+  {
+    id: 2,
+    title: "Chống quân xâm lược phương Bắc",
+  },
+  {
+    id: 3,
+    title: "Thời nhà Ngô - Đinh - Tiền Lê",
+  },
+  {
+    id: 4,
+    title: "Thời nhà Lý",
+  },
+  {
+    id: 5,
+    title: "Nhà Trần thắng giặc Nguyên Mông",
+  },
+  {
+    id: 6,
+    title: "Thời nhà Hồ",
+  },
+  {
+    id: 7,
+    title: "Khởi nghĩa Lam Sơn",
+  },
+  {
+    id: 8,
+    title: "Thời Lê Sơ",
+  },
+];
