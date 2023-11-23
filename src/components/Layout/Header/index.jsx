85dcd8ffa2237da00ab40d2bfb263ac31cd9becc
Le Hieu <firephoenix.shinji@gmail.com>
2023-11-24 04:22:10 +0700

    Change Navbar

--- src/components/Layout/Header/index.jsx
+++ src/components/Layout/Header/index.jsx
@@ -1,18 +1,22 @@
+"use client";
 import React from "react";
 import * as S from "./styles";
 import { FcSearch } from "react-icons/fc";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 function Header() {
+  const pathName = usePathname();
   return (
     <header>
       <S.NavWrapper
-        className="navbar navbar-expand-lg "
+        className="navbar navbar-expand-lg py-3 "
         style={{ backgroundColor: "#e3f2fd" }}
       >
         <div className="container">
-          <a className="navbar-brand" href="/">
+          <Link className="navbar-brand" href="/">
             Trang Chủ
-          </a>
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
@@ -26,26 +30,21 @@ function Header() {
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Sách Giáo Khoa
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/lich-su">
-                  Sách Lịch Sử
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/english">
-                  Sách Tiếng Anh
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/van-hoc">
-                  Sách Văn Học
-                </a>
-              </li>
+              {menu.map((item) => {
+                return (
+                  <li className="nav-item" key={item.id}>
+                    <Link
+                      className={
+                        pathName === item.href ? "nav-link active" : "nav-link"
+                      }
+                      aria-current="page"
+                      href={item.href}
+                    >
+                      {item.title}
+                    </Link>
+                  </li>
+                );
+              })}
             </ul>
             <form className="d-flex" role="search">
               <input
@@ -66,3 +65,26 @@ function Header() {
 }
 
 export default Header;
+
+const menu = [
+  {
+    id: 1,
+    href: "#",
+    title: "Sách Giáo Khoa",
+  },
+  {
+    id: 2,
+    href: "lich-su",
+    title: "Sách Lịch Sử",
+  },
+  {
+    id: 3,
+    href: "english",
+    title: "Sách Tiếng Anh",
+  },
+  {
+    id: 4,
+    href: "van-hoc",
+    title: "Sách Văn Học",
+  },
+];
