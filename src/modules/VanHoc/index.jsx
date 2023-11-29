1ea6f3faa2e7be1f249fc94ea88ce6cf36046028
Le Hieu <firephoenix.shinji@gmail.com>
2023-11-30 00:58:07 +0700

    Add Bai Viet

--- src/modules/VanHoc/index.jsx
+++ src/modules/VanHoc/index.jsx
@@ -8,6 +8,15 @@ function VanHoc() {
     <>
       <BannerPage title={"Tài Liệu Văn Học"} />
       <S.Wrapper className="container my-5">
+        <h3 className="my-3">Bài viết của học sinh qua các năm</h3>
+        <div>
+          <Link
+            href={"/pdf-book/vanhoc/bai-viet-cua-hs-qua-cac-nam.pdf"}
+            target="_blank"
+          >
+            Bài viết
+          </Link>
+        </div>
         <h3 className="my-3">Truyện Mợ Du</h3>
         <div>
           <Link href={"/pdf-book/vanhoc/mo-du.pdf"} target="_blank">
@@ -24,6 +33,48 @@ function VanHoc() {
         <div>
           <Link href={"/pdf-book/vanhoc/song-chet-mac-bay.pdf"} target="_blank">
             Sống chết mặc bay
+          </Link>
+        </div>
+        <h3 className="my-3">Truyện Nguyễn Nhật Ánh</h3>
+        <div>
+          <Link href={"/pdf-book/vanhoc/bi-an-cua-mot.pdf"} target="_blank">
+            Bí ẩn của Mốt
+          </Link>
+        </div>
+        <div className="mt-2">
+          <Link
+            href={"/pdf-book/vanhoc/cho-toi-mot-ve-di-tuoi-tho.pdf"}
+            target="_blank"
+          >
+            Cho tôi một vé đi tuổi thơ
+          </Link>
+        </div>
+        <div className="mt-2">
+          <Link
+            href={"/pdf-book/vanhoc/co-hai-con-meo-ben-cua-so.pdf"}
+            target="_blank"
+          >
+            Có hai con mèo ngồi bên cửa sổ
+          </Link>
+        </div>
+        <div className="mt-2">
+          <Link href={"/pdf-book/vanhoc/dao-mong-mo.pdf"} target="_blank">
+            Đảo mộng mơ
+          </Link>
+        </div>
+        <h3 className="my-3">Truyện Nguyễn Công Hoan</h3>
+        <div>
+          <Link href={"/pdf-book/vanhoc/dong-hao-co-ma.pdf"} target="_blank">
+            Đồng Hào có ma
+          </Link>
+        </div>
+        <h3 className="my-3">Truyện Nguyễn Ngọc Tư</h3>
+        <div>
+          <Link
+            href={"/pdf-book/vanhoc/truyen-ngan-nguyen-ngoc-tu.pdf"}
+            target="_blank"
+          >
+            Tập truyện ngắn của Nguyễn Ngọc Tư
           </Link>
         </div>
       </S.Wrapper>
