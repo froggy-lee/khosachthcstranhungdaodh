43ba2be98a23b794908e0509c3686524620443eb
Le Hieu <firephoenix.shinji@gmail.com>
2023-12-12 00:43:05 +0700

    Add Children Book

--- src/modules/Children/index.jsx
+++ src/modules/Children/index.jsx
@@ -8,73 +8,61 @@ function ChildrenPage() {
     <>
       <BannerPage title={"Sách Thiếu Nhi"} />
       <S.Wrapper className="container my-5">
-        <h3 className="my-3">Bài viết của học sinh qua các năm</h3>
+        <h3 className="my-3">Truyện Bà Chúa Tuyết</h3>
+        <div>
+          <Link href={"/pdf-book/childrens/ba-chua-tuyet.pdf"} target="_blank">
+            Bà Chúa Tuyết
+          </Link>
+        </div>
+        <h3 className="my-3">Truyện Bầy chim thiên nga</h3>
         <div>
           <Link
-            href={"/pdf-book/vanhoc/bai-viet-cua-hs-qua-cac-nam.pdf"}
+            href={"/pdf-book/childrens/bay-chim-thien-nga.pdf"}
             target="_blank"
           >
-            Bài viết
+            Bầy chim thiên nga
           </Link>
         </div>
-        <h3 className="my-3">Truyện Mợ Du</h3>
-        <div>
-          <Link href={"/pdf-book/vanhoc/mo-du.pdf"} target="_blank">
-            Mợ Du
-          </Link>
-        </div>
-        <h3 className="my-3">Truyện Những ngày thơ ấu</h3>
-        <div>
-          <Link href={"/pdf-book/vanhoc/nhung-ngay-tho-au.pdf"} target="_blank">
-            Những ngày thơ ấu
-          </Link>
-        </div>
-        <h3 className="my-3">Truyện Sống chết mặc bay</h3>
-        <div>
-          <Link href={"/pdf-book/vanhoc/song-chet-mac-bay.pdf"} target="_blank">
-            Sống chết mặc bay
-          </Link>
-        </div>
-        <h3 className="my-3">Truyện Nguyễn Nhật Ánh</h3>
-        <div>
-          <Link href={"/pdf-book/vanhoc/bi-an-cua-mot.pdf"} target="_blank">
-            Bí ẩn của Mốt
-          </Link>
-        </div>
-        <div className="mt-2">
-          <Link
-            href={"/pdf-book/vanhoc/cho-toi-mot-ve-di-tuoi-tho.pdf"}
-            target="_blank"
-          >
-            Cho tôi một vé đi tuổi thơ
-          </Link>
-        </div>
-        <div className="mt-2">
-          <Link
-            href={"/pdf-book/vanhoc/co-hai-con-meo-ben-cua-so.pdf"}
-            target="_blank"
-          >
-            Có hai con mèo ngồi bên cửa sổ
-          </Link>
-        </div>
-        <div className="mt-2">
-          <Link href={"/pdf-book/vanhoc/dao-mong-mo.pdf"} target="_blank">
-            Đảo mộng mơ
-          </Link>
-        </div>
-        <h3 className="my-3">Truyện Nguyễn Công Hoan</h3>
-        <div>
-          <Link href={"/pdf-book/vanhoc/dong-hao-co-ma.pdf"} target="_blank">
-            Đồng Hào có ma
-          </Link>
-        </div>
-        <h3 className="my-3">Truyện Nguyễn Ngọc Tư</h3>
+        <h3 className="my-3">Truyện Bộ quần áo mới của Hoàng Đế</h3>
         <div>
           <Link
-            href={"/pdf-book/vanhoc/truyen-ngan-nguyen-ngoc-tu.pdf"}
+            href={"/pdf-book/childrens/bo-quan-ao-moi-cua-hoang-de.pdf"}
             target="_blank"
           >
-            Tập truyện ngắn của Nguyễn Ngọc Tư
+            Bộ quần áo mới của Hoàng Đế
+          </Link>
+        </div>
+        <h3 className="my-3">Truyện Chú lính Chì dũng cảm</h3>
+        <div>
+          <Link
+            href={"/pdf-book/childrens/chu-linh-chi-dung-cam.pdf"}
+            target="_blank"
+          >
+            Chú lính Chì dũng cảm
+          </Link>
+        </div>
+        <h3 className="my-3">Truyện Cô bé bán diêm</h3>
+        <div>
+          <Link href={"/pdf-book/childrens/co-be-ban-diem.pdf"} target="_blank">
+            Cô bé bán diêm
+          </Link>
+        </div>
+        <h3 className="my-3">Truyện Cô bé tí hon</h3>
+        <div>
+          <Link href={"/pdf-book/childrens/co-be-ti-hon.pdf"} target="_blank">
+            Cô bé tí hon
+          </Link>
+        </div>
+        <h3 className="my-3">Truyện Nàng tiên cá</h3>
+        <div>
+          <Link href={"/pdf-book/childrens/nang-tien-ca.pdf"} target="_blank">
+            Nàng tiên cá
+          </Link>
+        </div>
+        <h3 className="my-3">Truyện Vịt con xấu xí</h3>
+        <div>
+          <Link href={"/pdf-book/childrens/vit-con-xau-xi.pdf"} target="_blank">
+            Vịt con xấu xí
           </Link>
         </div>
       </S.Wrapper>
